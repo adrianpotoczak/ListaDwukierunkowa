4740fbeb9ad0234ec9923815672bcf562e10ce5e
Ja <adrian.potoczak@wp.pl>
2025-10-21 00:38:27 +0200

    Stworzenie klasy węzeł do listy dwukierunkowej

---
+++ wezel.h
@@ -0,0 +1,15 @@
+#pragma once
+using namespace std;
+
+class wezel {
+public:
+    int dane;
+    wezel* nastepny;
+    wezel* poprzedni;
+
+    wezel(int wartosc) {
+        dane = wartosc;
+        nastepny = nullptr;
+        poprzedni = nullptr;
+    }
+};
